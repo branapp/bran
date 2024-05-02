8ee03199e0087ff30d679b0946f681dd9d23e515
tysonlmao <83587481+tysonlmao@users.noreply.github.com>
2024-05-02 18:52:53 +1000

    express base

---
+++ api/server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send({ "message": "why are you here" });
+});
+
+let port = 3000;
+app.listen(port, () => {
+    console.log('Server is running on port 3000');
+});
